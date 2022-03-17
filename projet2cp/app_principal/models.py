20394e838b8fa3064241b497e67bf7f0bff0948f
Amina <ka_bouzar@esi.dz>
2022-03-17 21:02:57 +0100

    added relation one-to-many Switch & Port tables

--- projet2cp/app_principal/models.py
+++ projet2cp/app_principal/models.py
@@ -3,37 +3,43 @@ from django.db import models
 # Create your models here.
 
 
-class switch(models.Model):
+class Switch(models.Model): #on peut creer les ports d'un switch a partir d'un objet "switch" : switch_objet.port_set.create(num_port=...,type_port=...etc)
     nom = models.CharField(max_length=50)
     marque = models.CharField(max_length=50)
     modele = models.CharField(max_length=50)
-    Bloc = models.CharField(max_length=10)
-    Armoire = models.CharField(max_length=10)
-    IP = models.GenericIPAddressField()
-    Inventaire = models.CharField(max_length=50)
-    Serie = models.CharField(max_length=20)
-    Mac = models.CharField(max_length=17)
-    Nbr_port = models.IntegerField(default=0)
-    Nbr_port_FE = models.IntegerField(default=0)
-    Nbr_port_GE = models.IntegerField(default=0)
-    Nbr_port_SFP = models.IntegerField(default=0)
+    bloc = models.CharField(max_length=10)
+    armoire = models.CharField(max_length=10)
+    ip = models.GenericIPAddressField()
+    inventaire = models.CharField(max_length=50)
+    serie = models.CharField(max_length=20)
+    mac = models.CharField(max_length=17)
+    nbr_port = models.IntegerField(default=0)
+    nbr_port_FE = models.IntegerField(default=0)
+    nbr_port_GE = models.IntegerField(default=0)
+    nbr_port_SFP = models.IntegerField(default=0)
     # l'element qui precede le switch (le nom du switch precendeant ou data center ou routeurs)
     preced = models.CharField(max_length=50)
     Vlans_associe = models.CharField(max_length=500)
     date_achat = models.DateTimeField('date d''achat')
 
+    def __str__(self):
+        return self.nom
 
-class vlan(models.Model):
-    num_Vlan = models.IntegerField()
+
+class Vlan(models.Model):
+    num_Vlan = models.IntegerField(default=0)
     nom = models.CharField(max_length=50)
-    Ip = models.GenericIPAddressField()
-    Masque = models.CharField(max_length=50)
-    Passerelle = models.GenericIPAddressField()
+    ip = models.GenericIPAddressField()
+    masque = models.CharField(max_length=50)
+    passerelle = models.GenericIPAddressField()
+
+    def __str__(self):
+        return self.nom
 
 
-class Port(models.Model):
-    Inventaire = models.CharField(max_length=50)
-    num_port = models.IntegerField()
+class Port(models.Model): 
+    switch= models.ForeignKey(Switch, on_delete=models.CASCADE) #afin que django va supprimer tous les ports associé à un switch si le switch est supprimé
+    num_port = models.IntegerField(default=0)
     type_port = models.CharField(max_length=50)
     etat = models.CharField(max_length=50)
     vlan_associe = models.CharField(max_length=50)
